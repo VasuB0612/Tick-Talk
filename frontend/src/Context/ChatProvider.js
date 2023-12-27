8b75209d0fd368414bbf0c3db5aa41688d5b840c
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2023-12-27 18:47:40 +0200

    Added context API for free state transfer.

---
+++ frontend/src/Context/ChatProvider.js
@@ -0,0 +1,32 @@
+import { createContext, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const ChatContext = createContext();
+
+const ChatProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+    console.log(userInfo);
+
+    if (!userInfo) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  return (
+    <ChatContext.Provider value={{ user, setUser }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChat = () => {
+  return useContext(ChatContext);
+};
+
+export default ChatProvider;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import ChatProvider from "./Context/ChatProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <ChatProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ChatProvider>
   </BrowserRouter>
 );
 

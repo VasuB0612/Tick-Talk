e71632dd5fb7cd233ef80436ad0651506ea3afdd
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2023-12-27 15:34:45 +0200

    added searching method

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { registerUser, authUser } = require("../Controllers/userController");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../Controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+router.route("/").post(protect, allUsers);
 router.route("/signup").post(registerUser);
 router.route("/login").post(authUser);
 

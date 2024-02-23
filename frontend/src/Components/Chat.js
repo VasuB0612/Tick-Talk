8f1b987d665e7173488cfa5566b0245bed0e3827
Vasu Bhatnagar <vasu.bhatnagar@tuni.fi>
2024-02-23 15:39:34 +0200

    Can see the texts in the chat box

--- frontend/src/Components/Chat.js
+++ frontend/src/Components/Chat.js
@@ -11,7 +11,7 @@ const Chat = ({ messages }) => {
       {messages &&
         messages.map((m, i) => (
           <div style={{ display: "flex" }} key={m._id}>
-            {isSameSender(messages, m, i, user._id) ||
+            {/* {isSameSender(messages, m, i, user._id) ||
             isLastSender(messages, i, user._id) ? (
               <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
                 <Avatar
@@ -24,8 +24,25 @@ const Chat = ({ messages }) => {
                 />
               </Tooltip>
             ) : (
-              console.log("Vasu")
-            )}
+              console.log("Cannot render")
+            )} */}
+            <span
+              style={{
+                backgroundColor: `${
+                  m.sender._id === user._id ? "#0088cc" : "#83f28f"
+                }`,
+                fontFamily: "Work Sans",
+                borderRadius: "20px",
+                padding: "0px 10px",
+                maxWidth: "75%",
+                color: "black",
+                marginBottom: "10px",
+                fontWeight: "bold",
+                fontSize: "small",
+              }}
+            >
+              {m.content}
+            </span>
           </div>
         ))}
     </ScrollableFeed>
